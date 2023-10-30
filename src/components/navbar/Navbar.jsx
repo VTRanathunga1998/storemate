74b1b5462a64378b8abec28b9bb240616fe609fa
Viraj Tharuka Ranathunga <virajtharuka.fb@gmail.com>
2023-10-30 09:06:06 +0530

    styles are added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import NavbarCSS from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [currentDate, setCurrentDate] = useState("");
+
+  useEffect(() => {
+    // Function to update the current date
+    const updateDate = () => {
+      const now = new Date();
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      const formattedDate = now.toLocaleDateString(undefined, options);
+      setCurrentDate(formattedDate);
+    };
+
+    // Initial update of the date
+    updateDate();
+
+    // Update the date every minute
+    const intervalId = setInterval(updateDate, 60000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <nav className={NavbarCSS.navbar}>
       <div className={NavbarCSS["nav-date"]}>
-        <span>2023/10/29</span>
+        <span>{currentDate}</span>
       </div>
       <div className={NavbarCSS["nav-brand"]}>
         <span>STOREMATE</span>
